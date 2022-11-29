30117f1b938782d09f0d2e52ae9a5181fcd9c258
Rencist <dafianakhdan@gmail.com>
2022-11-29 15:58:35 +0700

    refactor: configuration navbar n sidebad

--- resources/views/stock/all-stock.blade.php
+++ resources/views/stock/all-stock.blade.php
@@ -1,7 +1,6 @@
-@extends('layouts.auth')
+@extends('layouts.app')
 
 @section('content')
-    
 <div class="overflow-x-auto relative shadow-md sm:rounded-lg m-3">
     <table id="example" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -18,12 +17,15 @@
                 <th scope="col" class="py-3 px-6">
                     Harga Produk
                 </th>
+                <th scope="col" class="py-3 px-6">
+                    Buy Stock
+                </th>
             </tr>
         </thead>
         <tbody>
             @foreach ($stocks as $stocky => $stock)
                 <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-slate-100">
+                    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
                         {{ $stock["name"] }}
                     </th>
                     <td class="py-4 px-6">
@@ -34,6 +36,12 @@
                     </td>
                     <td class="py-4 px-6">
                         {{ $stock["harga"] }}
+                    </td>      
+                    <td>
+                        <button class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="authentication-modal">
+                            Buy
+                        </button>
+                        @include('stock.buy-stock')
                     </td>
                 </tr>
             @endforeach
